86c1532a9465d53e909c2b477cfeb0427cc774f6
muzamil <hussainmuzamil@gmail.com>
2025-02-02 12:47:53 +0500

    graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
+	"context"
 	"github.com/hussainmuzamil/students-api/internal/config"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	http.ListenAndServe(cfg.Addr,cfg.)
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("Starting server...", slog.String("addr", cfg.Addr))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	<-done
+	slog.Info("Shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shutdown server gracefully", slog.String("error", err.Error()))
+	}
+	slog.Info("Server gracefully stopped")
 }
- 
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,13 @@ package config
 
 import (
 	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"os"
-	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true" `
 }
 
 type Config struct {
@@ -22,25 +22,25 @@ func MustLoad() *Config {
 
 	configPath = os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		flags := flag.String("config","","path to the configuration file")
+		flags := flag.String("config", "", "path to the configuration file")
 		flag.Parse()
 
 		configPath = *flags
 
-		if(configPath == ""){
+		if configPath == "" {
 			log.Fatal("Config Path is not set")
 		}
 	}
 
-	if _,err := os.Stat(configPath); os.IsNotExist(err){
-		log.Fatalf("Config file doesn't exist: %s",configPath)
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file doesn't exist: %s", configPath)
 	}
 
 	var cfg Config
 
-	err := cleanenv.ReadConfig(configPath,&cfg)
-	if err != nil{
-		log.Fatalf("Unable to Parse Config File: %s",err.Error())
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("Unable to Parse Config File: %s", err.Error())
 	}
 
 	return &cfg
